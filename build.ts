753dc16657595a3712cff042c6d8ded27df2f08e
genzj <zj0512@gmail.com>
2024-09-13 16:32:36 -0700

    build: :package: refactor the build script to prepare for the file watch feature

--- build.ts
+++ build.ts
@@ -2,7 +2,6 @@ import styleLoader from "bun-style-loader";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-import { promises as fs } from "fs";
 import path from "path";
 import type { PackageJson } from "type-fest";
 
@@ -18,7 +17,7 @@ interface ScriptOptions {
   entrypointPath: string;
 }
 
-export async function postBuildScript(options: ScriptOptions): Promise<void> {
+async function postBuildScript(options: ScriptOptions): Promise<void> {
   const { entrypointPath } = options;
   const packageJsonPath = "./package.json";
 
@@ -56,55 +55,70 @@ export async function postBuildScript(options: ScriptOptions): Promise<void> {
 
 `;
 
-  const data = await fs.readFile(entrypointPath);
   const outputPath = `${path.dirname(entrypointPath)}/${distUserScript}`;
-  const fd = await fs.open(outputPath, "w");
-  await fd.write(HEADER_BEGIN);
+  const data = await Bun.file(entrypointPath).text();
+  let output = HEADER_BEGIN;
+
   for (const header in packageJson.userscriptHeader) {
     const value = packageJson.userscriptHeader[header];
     for (const row of typeof value === "string" ? [value] : value) {
-      await fd.write(`// ${header} ${row}\n`);
+      output += `// ${header} ${row}\n`;
     }
   }
-  await fd.write(HEADER_END);
-  await fd.write(data);
-  await fd.close();
+  output += HEADER_END;
+  output += data;
+
+  await Bun.write(outputPath, output);
+  console.info(
+    `Successfully added the header to the userscript ${outputPath}!`
+  );
 }
 
-const argv = await yargs(hideBin(process.argv))
-  .option("dev", {
-    type: "boolean",
-    description:
-      "Build in development mode, which disables minify and enables inline source map",
-    default: false,
-  })
-  .parse();
-
-const build = await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: "./dist",
-  format: "esm",
-  minify: !argv.dev,
-  sourcemap: argv.dev ? "inline" : undefined,
-  loader: {
-    ".html": "text",
-  },
-  plugins: [styleLoader()],
-});
-
-console.info(build);
-
-if (!build.success) {
-  process.exit(1);
+interface BuildOptions {
+  dev: boolean;
 }
 
-const entrypointPath = build.outputs.find(
-  (artifact) => artifact.kind === "entry-point"
-)?.path;
-console.info(`Running post build script with entrypoint ${entrypointPath}.`);
-if (!entrypointPath) {
-  throw new Error("cannot find entrypoint in built artifacts.");
+async function build(params: BuildOptions) {
+  const { dev } = params;
+  const build = await Bun.build({
+    entrypoints: ["./src/index.ts"],
+    outdir: "./dist",
+    minify: !dev,
+    sourcemap: dev ? "inline" : undefined,
+    loader: {
+      ".html": "text",
+    },
+    plugins: [styleLoader()],
+  });
+
+  console.info(build);
+
+  if (!build.success) {
+    throw new Error("Bun build return errors");
+  }
+
+  const entrypointPath = build.outputs.find(
+    (artifact) => artifact.kind === "entry-point"
+  )?.path;
+  console.info(`Running post build script with entrypoint ${entrypointPath}.`);
+  if (!entrypointPath) {
+    throw new Error("cannot find entrypoint in built artifacts.");
+  }
+
+  await postBuildScript({ entrypointPath });
 }
 
-await postBuildScript({ entrypointPath });
-console.info("Successfully added the header to the userscript!");
+async function main() {
+  const argv = await yargs(hideBin(process.argv))
+    .option("dev", {
+      type: "boolean",
+      description:
+        "Build in development mode, which disables minify and enables inline source map",
+      default: false,
+    })
+    .parse();
+
+  await build({ dev: argv.dev });
+}
+
+await main();
